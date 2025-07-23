4276a337cfdcabe0802ec1a8fe63e04481441eba
sarikanaphade <sarikanaphade@gmail.com>
2025-07-22 21:17:30 -0700

    Added Create Contact test cases

--- pages/ContactPage.js
+++ pages/ContactPage.js
@@ -1,16 +1,49 @@
+
 const { expect } = require('@playwright/test');
 class ContactPage{
 
+    constructor(page){
+        this.page = page;
+        this.organizaton = page.locator('input[name="organizationName"]');
+        this.title = page.locator('input[name="title"]');
+        this.department = page.locator('input[name="department"]');
+        this.officephone = page.locator('input[name="officePhone"]');
+        this.contactname = page.locator('input[name ="contactName"]');
+        this.mobile = page.locator('input[name="mobile"]');
+        this.email = page.locator('input[name="email"]');
+        this.campaign = page.locator("//div[@class='form-group']//div//button[@type='button']");
+        this.createContactButton = page.locator("//button[@type='submit']");   
+        this.tooltipMessage=page.locator("//div[@role='alert']");
+        this.searchOptionSelect=page.locator("//select[@class='form-control']");
+        this.searchInput = page.getByPlaceholder("Search by Contact Name");
+        this.searchTable = page.locator("//table[@class='table table-striped table-hover']/tbody/tr[1]");
     }
 
-    async fillmandatoryFields(organization, title, department, officePhone, contactName, mobile, email) {
+    async fillAllFields(organization, title, department, officePhone, contactName, mobile, email, campaign) {
+        await this.organizaton.fill(organization);
+        await this.title.fill(title);
+        await this.department.fill(department);
+        await this.officephone.fill(officePhone);
+        await this.contactname.fill(contactName);
+        await this.mobile.fill(mobile);
+        await this.email.fill(email);
+        await this.campaign.fill(campaign);
+    }
+
+    async fillmandatoryFields(organization, title, department, officePhone, contactName, mobile, email,campaignName) {
         await this.organizaton.fill(organization);
         await this.title.fill(title);
         await this.contactname.fill(contactName);
         await this.mobile.fill(mobile);
+        const campaignPromise = this.page.waitForEvent('popup');
         await this.campaign.click();
-        const campaignOption = this.page.locator("//tbody/tr[2]/td[7]/button[1]");
-        await campaignOption.click();
+        const campaignSearchPage = await campaignPromise;
+        await campaignSearchPage.locator('#search-criteria').selectOption('campaignName');
+        await campaignSearchPage.locator("#search-input").fill(campaignName)
+        const actCampaingName = await campaignSearchPage.locator("#campaign-table tbody > tr:nth-child(1)").locator('td:nth-child(2)').textContent()
+        expect(actCampaingName).toEqual(campaignName);
+        await campaignSearchPage.locator("//button[text()='Select']").first().click();;
+        // await campaignSearchPage.pause();
     }
     
     async fillnonMandatoryFields(department, officePhone, email) {
@@ -20,7 +53,25 @@ class ContactPage{
     }
     
     async clickAddButton() {
-        await this.createContactButton.click();
+        await this.createContactButton.click();        
     }
+
+    async verifyMessage(){
+        const messageText = await this.tooltipMessage.textContent();
+        console.log(messageText);
+        expect(messageText).toContain('Successfully Added');
+      }
+
+      async searchContact(contactname,mobile){
+        await this.searchOptionSelect.selectOption("contactName");
+        await this.searchInput.fill(contactname);
+        // const rcount = await this.searchTable.count();
+        // expect(rcount).toBe(1);
+        const pname = await this.searchTable.locator('td:nth-child(2)').textContent();
+        expect(pname.trim()).toEqual(prodname);
+        const mobileno = await this.searchTable.locator('td:nth-child(7)').textContent();
+        expect(mobileno.trim()).toEqual(mobile);
+
+     }
 }
 module .exports = { ContactPage };
